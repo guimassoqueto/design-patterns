a4a7bd0736dba35cbe765b0f174cb0ff36554f4f
guimassoqueto <gui.massoqueto@gmail.com>
2023-05-05 17:42:17 -0300

    style: update indentation

--- Creational - Factory/Java/Main.java
+++ Creational - Factory/Java/Main.java
@@ -6,19 +6,19 @@ public class Main {
   private static Dialog dialog;
 
   public static void main(String[] args) {
-      configure();
-      runBusinessLogic();
+    configure();
+    runBusinessLogic();
   }
 
   static void configure() {
-      if (System.getProperty("os.name").equals("Windows 10")) {
-          dialog = new WindowsDialog();
-      } else {
-          dialog = new HtmlDialog();
-      }
+    if (System.getProperty("os.name").equals("Windows 10")) {
+    dialog = new WindowsDialog();
+    } else {
+    dialog = new HtmlDialog();
+    }
   }
 
   static void runBusinessLogic() {
-      dialog.renderWindow();
+    dialog.renderWindow();
   }
 }
