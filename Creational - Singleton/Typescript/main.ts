3393bb9e645c24dfe76ebff77fc8d7794149f8fb
guimassoqueto <gui.massoqueto@gmail.com>
2023-06-03 17:00:51 -0300

    refactor: singleton - typescript

--- Creational - Singleton/Typescript/main.ts
+++ Creational - Singleton/Typescript/main.ts
@@ -1,33 +1,15 @@
-class Singleton {
-  private static instance: Singleton;
+import { Singleton } from "./singleton/singleton.ts";
 
-  private constructor() { }
 
-  public static getInstance(): Singleton {
-    if (!Singleton.instance) {
-      Singleton.instance = new Singleton();
-    }
-
-    return Singleton.instance;
-  }
-
-  public someBusinessLogic() {
-    // ...
-  }
-}
-
-/**
-* The client code.
-*/
-function clientCode() {
+function main () {
   const s1 = Singleton.getInstance();
   const s2 = Singleton.getInstance();
 
   if (s1 === s2) {
-      console.log('Singleton works, both variables contain the same instance.');
+    console.log('Singleton works, both variables contain the same instance.');
   } else {
-      console.log('Singleton failed, variables contain different instances.');
+    console.log('Singleton failed, variables contain different instances.');
   }
 }
 
-clientCode();
+main();
