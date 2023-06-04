e9103f3f200f43b280c36dc88c961280d05d5a6c
guimassoqueto <gui.massoqueto@gmail.com>
2023-06-04 09:29:34 -0300

    refactor: composite - typescript

---
+++ Structural - Composite/Typescript/leaf/leaf.ts
@@ -0,0 +1,8 @@
+import { Component } from "../component/component.ts";
+
+
+export class Leaf extends Component {
+  public operation(): string {
+    return 'Leaf';
+  }
+}
--- Structural - Composite/Typescript/main.ts
+++ Structural - Composite/Typescript/main.ts
@@ -1,68 +1,11 @@
-abstract class Component {
-  protected parent!: Component | null;
-
-  public setParent(parent: Component | null) {
-      this.parent = parent;
-  }
-
-  public getParent(): Component | null {
-      return this.parent;
-  }
-
-  public add(component: Component): void { }
-
-  public remove(component: Component): void { }
-
-  public isComposite(): boolean {
-      return false;
-  }
-
-  public abstract operation(): string;
-}
-
-
-class Leaf extends Component {
-  public operation(): string {
-      return 'Leaf';
-  }
-}
-
-
-class Composite extends Component {
-  protected children: Component[] = [];
-
-  public add(component: Component): void {
-      this.children.push(component);
-      component.setParent(this);
-  }
-
-  public remove(component: Component): void {
-      const componentIndex = this.children.indexOf(component);
-      this.children.splice(componentIndex, 1);
-
-      component.setParent(null);
-  }
-
-  public isComposite(): boolean {
-      return true;
-  }
-
-  public operation(): string {
-      const results: string[] = [];
-      for (const child of this.children) {
-          results.push(child.operation());
-      }
-
-      return `Branch(${results.join('+')})`;
-  }
-}
+import { Component } from "./component/component.ts";
+import { Composite } from "./composite/composite.ts";
+import { Leaf } from "./leaf/leaf.ts";
 
 
 function clientCode(component: Component) {
   // ...
-
   console.log(`RESULT: ${component.operation()}`);
-
   // ...
 }
 
@@ -88,12 +31,10 @@ console.log('');
 
 function clientCode2(component1: Component, component2: Component) {
   // ...
-
   if (component1.isComposite()) {
       component1.add(component2);
   }
   console.log(`RESULT: ${component1.operation()}`);
-
   // ...
 }
 
