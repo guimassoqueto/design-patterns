f110fb2830f208710953a05b59926f9242f600d1
guimassoqueto <gui.massoqueto@gmail.com>
2023-06-04 09:25:59 -0300

    refactor: composite - python

--- Structural - Composite/Python/main.py
+++ Structural - Composite/Python/main.py
@@ -1,63 +1,13 @@
-from __future__ import annotations
-from abc import ABC, abstractmethod
-from typing import List
+from component.component import Component
+from composite.composite import Composite
+from leaf.leaf import Leaf
 
 
-class Component(ABC):
-    @property
-    def parent(self) -> Component:
-        return self._parent
-
-    @parent.setter
-    def parent(self, parent: Component):
-        self._parent = parent
-
-    def add(self, component: Component) -> None:
-        pass
-
-    def remove(self, component: Component) -> None:
-        pass
-
-    def is_composite(self) -> bool:
-        return False
-
-    @abstractmethod
-    def operation(self) -> str:
-        pass
-
-
-class Leaf(Component):
-    def operation(self) -> str:
-        return "Leaf"
-
-
-class Composite(Component):
-    def __init__(self) -> None:
-        self._children: List[Component] = []
-
-    def add(self, component: Component) -> None:
-        self._children.append(component)
-        component.parent = self
-
-    def remove(self, component: Component) -> None:
-        self._children.remove(component)
-        component.parent = None
-
-    def is_composite(self) -> bool:
-        return True
-
-    def operation(self) -> str:
-        results = []
-        for child in self._children:
-            results.append(child.operation())
-        return f"Branch({'+'.join(results)})"
-
-
-def client_code(component: Component) -> None:
+def main(component: Component) -> None:
     print(f"RESULT: {component.operation()}", end="")
 
 
-def client_code2(component1: Component, component2: Component) -> None:
+def main2(component1: Component, component2: Component) -> None:
     if component1.is_composite():
         component1.add(component2)
 
@@ -67,7 +17,7 @@ def client_code2(component1: Component, component2: Component) -> None:
 if __name__ == "__main__":
     simple = Leaf()
     print("Client: I've got a simple component:")
-    client_code(simple)
+    main(simple)
     print("\n")
 
     tree = Composite()
@@ -83,8 +33,9 @@ if __name__ == "__main__":
     tree.add(branch2)
 
     print("Client: Now I've got a composite tree:")
-    client_code(tree)
+    main(tree)
     print("\n")
 
     print("Client: I don't need to check the components classes even when managing the tree:")
-    client_code2(tree, simple)
+    main2(tree, simple)
+    print("\n")
