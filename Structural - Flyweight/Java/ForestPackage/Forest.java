67673b0082c227ef688a788d907502c12569f45d
guimassoqueto <gui.massoqueto@gmail.com>
2023-06-26 08:20:29 -0300

    feat: flyweight - java

---
+++ Structural - Flyweight/Java/ForestPackage/Forest.java
@@ -0,0 +1,29 @@
+package ForestPackage;
+
+import javax.swing.*;
+
+import TreesPackage.Tree;
+import TreesPackage.TreeFactory;
+import TreesPackage.TreeType;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Forest extends JFrame {
+    private List<Tree> trees = new ArrayList<>();
+
+    public void plantTree(int x, int y, String name, Color color, String otherTreeData) {
+        TreeType type = TreeFactory.getTreeType(name, color, otherTreeData);
+        Tree tree = new Tree(x, y, type);
+        trees.add(tree);
+    }
+
+    @Override
+    public void paint(Graphics graphics) {
+        for (Tree tree : trees) {
+            tree.draw(graphics);
+        }
+    }
+}
